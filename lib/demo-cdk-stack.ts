4b543b3b5b3a563260d765315f0b8694792cc282
Ehsan sarshar <ehsan.herai@gmail.com>
2022-01-23 01:08:26 +0430

    initial demo commit

---
+++ lib/demo-cdk-stack.ts
@@ -0,0 +1,20 @@
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apiGW from 'aws-cdk-lib/aws-apigateway'
+
+export class DemoCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    })
+
+    new apiGW.LambdaRestApi(this, 'EndPoint', {
+      handler: hello
+    })
+  }
+}
